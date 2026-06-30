7fea52c42f05960051fd815a29ead28d51235a43
ramiledov <ramiledov@gmail.com>
2026-06-30 15:26:49 +0400

    Export classes and list of students in ES6 module format

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,4 +1,4 @@
-class HolbertonClass {
+export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -13,7 +13,7 @@ class HolbertonClass {
   }
 }
 
-class StudentHolberton {
+export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -33,7 +33,7 @@ class StudentHolberton {
   }
 }
 
-// Instances (must come AFTER class definitions)
+// Instances
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -43,6 +43,10 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-const listOfStudents = [student1, student2, student3, student4, student5];
-
-export default listOfStudents;
+export const listOfStudents = [
+  student1,
+  student2,
+  student3,
+  student4,
+  student5
+];
