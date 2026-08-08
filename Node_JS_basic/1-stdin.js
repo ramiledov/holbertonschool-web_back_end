47669c27418b5222ee1ddfcac4cabea65feeb7f1
ramiledov <ramiledov@gmail.com>
2026-08-08 14:30:45 +0400

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,10 +4,10 @@ process.stdin.on('readable', () => {
   const input = process.stdin.read();
 
   if (input !== null) {
-    process.stdout.write(`Your name is: ${input.toString().trim()}\n`);
+    process.stdout.write(`Your name is: ${input.toString().replace('\n', '')}`);
   }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write('\nThis important software is now closing\n');
 });
